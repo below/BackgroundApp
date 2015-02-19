df6b8d4d0ce99836c1560df718b4a568cdc6edce
Alexander von Below <Alex@vonBelow.Com>
2015-02-19 18:43:59 +0100

    Initial Commit

---
+++ Background AppTests/Background_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Background_AppTests.swift
+//  Background AppTests
+//
+//  Created by Alexander v. Below on 19.02.15.
+//  Copyright (c) 2015 Alexander von Below. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Background_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
